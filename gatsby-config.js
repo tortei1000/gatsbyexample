65d4ee1564919ee8537cd35f99149ede6a5754a0
tiago maffei <tiagomaffei@tiagos-MacBook-Pro.local>
2019-05-22 15:15:11 -0600

    gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,24 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Pandalalalala`,
+    footer: `footer is here`
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
